a5154e7c016d52367a772b2b800e36fc2d523ba9
MarkKurzeja <mtkurzej@umich.edu>
2018-11-26 00:17:18 -0500

    Added Some fun new graphics

--- code/01_Data_Mugger.R
+++ code/01_Data_Mugger.R
@@ -8,9 +8,10 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(forcats)
+library(stringr)
 
 
- setwd("C:/Users/mtkur/Dropbox/Graduate School/05) Courses/SI 671/Personal-Finance-Predictions")
+ setwd("C:/Users/Mark/Dropbox/Graduate School/05) Courses/SI 671/Personal-Finance-Predictions")
 
 data <- read.table("./data/data_20181112.txt", sep = "\t", comment.char = "#", fill = T, header = T)
 
@@ -27,13 +28,22 @@ mdat <- ndat %>%
   group_by(Year = year(Date), Month = month(Date)) %>%
   summarize(Monthly = sum(Outflow))
 
+# Plot for the project
+mdat %>% 
+  mutate(Date = lubridate::make_date(Year, Month, 1)) %>% 
+  ggplot() + 
+    geom_line(aes(Date, Monthly)) + 
+  ggtitle("Spending on One Particular Category since July 2016") +
+  labs(y = "Monthly Spending") + 
+  ggsave(path = "./figures/", device = "png", filename = "Spending_Example.png")
+
 # Loss function
 myloss <- function(actual, error) {
   return(abs(actual - error))
 }
 
 # Baselines - Lookback Average
-all_opts = expand.grid(mend = 5:(length(mdat$Year) - 1), lookback = 1:15)
+all_opts = expand.grid(mend = 5:(length(mdat$Year) - 1), lookback = 0:15)
 
 baseline_results <- adply(.data = all_opts, .margins = 1, .fun = function(x) {
   mend = x$mend
@@ -47,11 +57,14 @@ baseline_results %<>%
   group_by(Type) %>% 
   summarize(error = mean(error))
 
+xtable::xtable(baseline_results)
+
 baseline_results %>% 
   ggplot() + 
   geom_point(aes(y = fct_rev(Type), x = error)) + 
   labs(y = "Type", x = "Mean Error") + 
   ggtitle("Mean Prediction Error for Baseline", "Using the Mean to Predict k + 1")
+  ggsave(path = "./figures/", device = "png", filename = "Lagged_Mean_Prediction_Results.png")
 
 # Arima Output 
 all_opts = expand.grid(mend = 5:(length(mdat$Year) - 1), one = c(0,1), two = 0, three = 0:2)
@@ -70,5 +83,7 @@ arima_results %<>%
 
 arima_results %>% 
   ggplot() + 
-  geom_point(aes(x = error, y = Type))
+  geom_point(aes(x = error, y = Type)) +
+  ggtitle("Arima Modeling Error")
+ggsave(path = "./figures/", device = "png", filename = "Arima_Modeling_Error_Init.png")
 
